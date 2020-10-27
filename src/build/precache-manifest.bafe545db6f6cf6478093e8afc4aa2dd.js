79aa24193b96fd1508642bc06e857f4aec6e0179
jimc <jimc@jimcs-MacBook-Pro.local>
2020-10-27 13:46:53 +0800

    First Commit

---
+++ src/build/precache-manifest.bafe545db6f6cf6478093e8afc4aa2dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40fa2c919fda987f049689d12e2951fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eae9512be851ef17214b",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "4e1bdb0adf4ac8796ad9",
+    "url": "/static/js/2.52bc9b7c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.52bc9b7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eae9512be851ef17214b",
+    "url": "/static/js/main.62090329.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
